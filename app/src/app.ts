425f58e814584f41b324ef9a49a7e556e47ec8ee
Lukasz <7lukasz8@gmail.com>
2024-04-21 23:24:30 +0200

    cros fix

--- app/src/app.ts
+++ app/src/app.ts
@@ -3,19 +3,13 @@ import express from "express";
 import loginRouter from "./routes/loginRouter";
 import officeRouter from "./routes/officeRouter";
 import cors from "cors";
-//const singUpRouter = require('./routes/singUpRouter')
 
 const app = express();
 
-const corsOptions = {
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/login", loginRouter);
 app.use("/api/v1/office", officeRouter);
-//app.use('/api/v1/sing-up', singUpRouter)
 
 export default app;
