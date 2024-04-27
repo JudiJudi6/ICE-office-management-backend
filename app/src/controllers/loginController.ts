1746fb6eff517f7ccf2461c9243e3b033b3a2e0c
mnaboo <maciek@nabozny.pl>
2024-04-27 13:59:33 +0200

    signup and login added

--- app/src/controllers/loginController.ts
+++ app/src/controllers/loginController.ts
@@ -17,13 +17,13 @@ export const getAllUsers: (req: Request, res: Response) => Promise<void> = async
 export const login = async (req: Request, res: Response) => {
 
     try {
-        const { email, password } = req.body;
+        const { mail, password } = req.body;
 
-        if (!email || !password) {
+        if (!mail || !password) {
             return res.status(400).json({ error: "Email and password are required." });
         }
 
-        const user = await userModel.findOne({ email }).select('+authentication.password +authentication.salt +authentication.sessionToken')
+        const user = await userModel.findOne({ mail }).select('+authentication.password +authentication.salt +authentication.sessionToken')
 
         if (!user) {
             return res.status(400).json({ error: "User not found." });
