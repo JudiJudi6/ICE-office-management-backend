11a53ef7f07863c9b01c37834bba7e4730f2ec35
Lukasz <7lukasz8@gmail.com>
2024-05-01 19:15:32 +0200

    get user post method

--- app/src/routes/loginRouter.ts
+++ app/src/routes/loginRouter.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { login } from "../controllers/loginController";
+import { getUser, login } from "../controllers/loginController";
 
 const router = express.Router();
 
 router.route("/").post(login);
+router.route("/getUser").post(getUser)
 
 export default router;
